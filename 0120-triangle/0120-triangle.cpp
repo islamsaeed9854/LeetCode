987782a800b475d645b0268b39c1d956fcb79447
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2025-07-29 15:27:18 +0300

    Time: 4 ms (7.94%), Space: 14 MB (7.85%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,17 +3,17 @@ public:
    int solve(int row, int col,int rows, vector<vector<int>>& triangle, vector < vector<int>>&memo) {
     if (row >= rows-1)return triangle[row][col];
     if (col<0 || col> row)return 1e9;
-    if (memo[row][col] != 1e9)
+    if (memo[row][col] != -2)
         return memo[row][col];
 
-    
-    int m1 =  triangle[row][col]  +  solve(row + 1, col , rows , triangle,memo);
-    int m2 =  triangle[row][col]  +  solve(row + 1, col + 1,rows,triangle,memo);
+    int mx = 1e9;
+    mx = min(mx, triangle[row][col]  +  solve(row + 1, col , rows , triangle,memo));
+    mx = min(mx, triangle[row][col]  +  solve(row + 1, col + 1,rows,triangle,memo));
    
-    return memo[row][col] = min(m1,m2);
+    return memo[row][col] = mx;
 }
 int minimumTotal(vector<vector<int>>& triangle) {
-    vector < vector<int>>memo(205,vector<int>(205, 1e9 ));
+    vector < vector<int>>memo(205,vector<int>(triangle.size(),-2));
     return solve(0, 0,triangle.size(),triangle,memo);
 }
 };
