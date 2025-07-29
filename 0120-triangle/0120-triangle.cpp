fed7326428f9f0067a86e1421daee068f7f39922
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2025-07-29 15:25:34 +0300

    Time: 11 ms (3.81%), Space: 23.9 MB (7.85%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-   int solve(int i, int j, int n, vector<vector<int>>&triangle, vector<vector<int>>&dp) {
-		if(i == n - 1)
-			return triangle[i][j];
-		if(dp[i][j] != -2)
-			return dp[i][j];
-		int bottom = triangle[i][j] + solve(i+1, j, n, triangle, dp);
-		int  bottom_right = triangle[i][j] + solve(i+1, j+1, n, triangle, dp);
-		return dp[i][j] = min(bottom, bottom_right);
-	}
-	int minimumTotal(vector<vector<int>>& triangle) {
-		int n = triangle.size();
-		vector<vector<int>>dp(n, vector<int>(n, -2));
-		return solve(0, 0, n, triangle, dp);
-	}
+   int solve(int row, int col,int rows, vector<vector<int>>& triangle, vector < vector<int>>&memo) {
+    if (row >= rows-1)return triangle[row][col];
+    if (col<0 || col> row)return 1e9;
+    if (memo[row][col] != 1e9)
+        return memo[row][col];
+
+    
+    int m1 =  triangle[row][col]  +  solve(row + 1, col , rows , triangle,memo);
+    int m2 =  triangle[row][col]  +  solve(row + 1, col + 1,rows,triangle,memo);
+   
+    return memo[row][col] = min(m1,m2);
+}
+int minimumTotal(vector<vector<int>>& triangle) {
+    vector < vector<int>>memo(205,vector<int>(205, 1e9 ));
+    return solve(0, 0,triangle.size(),triangle,memo);
+}
 };
