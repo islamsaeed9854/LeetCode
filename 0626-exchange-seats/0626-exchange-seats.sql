357086c5cdbe17efde35d09cf183fb91da6794fb
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2025-08-08 18:17:51 +0300

    Time: 359 ms (36.19%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,13 +1,10 @@
-# Write your MySQL query statement below
-/* Write your T-SQL query statement below */
 
 
 
 
-
-
-select s.id ,  (case when  s.id%2 = 0 then (select s1.student from Seat s1 where s1.id = s.id-1) 
-              when  (select count(s.id)
-from Seat s) != s.id and s.id%2 != 0   then (select s1.student from Seat s1 where s1.id = s.id+1) 
-              else s.student end ) as student 
-from Seat s
+select st.id , ( case when  st.id%2 = 0 then prev.student 
+                   when  st.id%2 != 0 and (nxt.id) is not null then nxt.student
+                   else  st.student end) as student 
+from seat st
+left outer join seat prev on prev.id = st.id - 1
+left outer join seat nxt on nxt.id = st.id + 1
