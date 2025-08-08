106ee4e69183a29e4616153a21daa0b2ceb20056
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2025-08-08 18:18:20 +0300

    Time: 349 ms (46.92%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,10 +1,11 @@
-
-
-
-
-select st.id , ( case when  st.id%2 = 0 then prev.student 
-                   when  st.id%2 != 0 and (nxt.id) is not null then nxt.student
-                   else  st.student end) as student 
-from seat st
-left outer join seat prev on prev.id = st.id - 1
-left outer join seat nxt on nxt.id = st.id + 1
+SELECT 
+    s.id,
+    CASE 
+        WHEN s.id % 2 = 0 THEN prev.student
+        WHEN s.id % 2 = 1 AND s.id < (SELECT MAX(id) FROM Seat) THEN next.student
+        ELSE s.student
+    END AS student
+FROM Seat s
+LEFT JOIN Seat prev ON s.id = prev.id + 1
+LEFT JOIN Seat next ON s.id = next.id - 1
+ORDER BY s.id;
