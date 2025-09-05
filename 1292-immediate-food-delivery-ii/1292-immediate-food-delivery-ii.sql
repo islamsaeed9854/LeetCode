819a7aa78afcf46cd53d0936458b40fe9597d619
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2025-09-05 17:16:36 +0300

    Time: 941 ms (68.29%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,8 +1,12 @@
-# Write your MySQL query statement belo
 
-with T as(
-select *
-from Delivery D
-where order_date = (select D2.order_date from Delivery D2 where D.customer_id  = D2.customer_id order by D2.order_date limit 1 ) 
+
+with T as
+(
+    select *, row_number() over  (partition by customer_id order by order_date  ) as RN
+    from Delivery 
 )
-select round( 100.0*sum(case when order_date  = customer_pref_delivery_date   then 1 else 0 end ) /count(*),2) immediate_percentage  from T 
+select round(100.0*sum(case when order_date  = customer_pref_delivery_date  then  1 else 0 end) /count(*),2) immediate_percentage 
+from T 
+where RN = 1
+
+
