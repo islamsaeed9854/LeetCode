2f4cd09d4a3ae6abd691829b3d242a1b85358eae
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2025-09-05 08:15:55 +0300

    Time: 259 ms (35.24%), Space: 0B (100%) - LeetHub

---
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+select machine_id  , round(sum(case when activity_type ='end' then timestamp 
+else -timestamp end)/(count(*)/2),3) processing_time 
+from Activity 
+group by machine_id 
