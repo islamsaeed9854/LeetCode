b38508d579a4508bbff84c4031a66b74fb03a0ad
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2025-09-07 13:50:19 +0300

    Time: 866 ms (82.48%), Space: 0B (100%) - LeetHub

--- 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
 
-select user_id , upper(substring(name,1,1)) + lower(substring(name,2,len(name)-1)) name
+select user_id , concat(upper(substring(name,1,1)),  lower(substring(name,2,len(name)-1))) name
 from Users 
 order by user_id
