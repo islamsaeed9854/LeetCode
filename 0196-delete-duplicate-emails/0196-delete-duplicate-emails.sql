6a749c961fad21ec7ad76a7b45e04786437fd835
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2025-09-07 14:25:47 +0300

    Time: 412 ms (89.61%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,10 @@
 /* Write your T-SQL query statement below */
 
 
-delete P1 from Person P1
-where email in (select email from  Person P where P.id < P1.id and P.email = P1.email )
+WITH CTE AS (
+    SELECT id,email,ROW_NUMBER() OVER(PARTITION BY email ORDER BY id) AS rn
+    FROM Person
+)
+DELETE P1 FROM Person P1 join CTE C
+on  P1.id = C.id
+WHERE rn > 1;
