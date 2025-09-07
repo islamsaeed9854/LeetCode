5779903cb0b31415ac5f7a407a5d51835d8653e1
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2025-09-07 15:20:06 +0300

    Time: 874 ms (48.73%), Space: 0B (100%) - LeetHub

--- 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.sql
+++ 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.sql
@@ -1,6 +1,6 @@
 select a.sell_date
    , count(a.product) num_sold
-   , string_agg(a.product,',') within group (order by a.product) products
+   , string_agg(a.product,',')  products
 from (select distinct sell_date,product from activities) a
 group by a.sell_date
 order by a.sell_date
