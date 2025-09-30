bc06f566bda357a95615f9870b1d7354096fc6aa
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2025-09-30 15:10:30 +0300

    Time: 308 ms (91.44%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,11 +1,10 @@
-SELECT 
-    s.id,
-    CASE 
-        WHEN s.id % 2 = 0 THEN prev.student
-        WHEN s.id % 2 = 1 AND s.id < (SELECT MAX(id) FROM Seat) THEN next.student
-        ELSE s.student
-    END AS student
-FROM Seat s
-LEFT JOIN Seat prev ON s.id = prev.id + 1
-LEFT JOIN Seat next ON s.id = next.id - 1
-ORDER BY s.id;
+-- 1  | Abbot     2  | Doris      null null
+-- 2  | Doris     3  | Emerson    1    Abbot
+-- 3  | Emerson   4  | Green      2    Doris
+-- 4  | Green     5  | Jeames     3    Emerson
+-- 5  | Jeames    null null       4    Green
+with CTE as(
+    select count(*) CNT from Seat 
+)
+select s1.id, (case when s1.id % 2 = 0 then s3.student when s1.id%2!=0 and s1.id!= (select CNT from CTE ) then s2.student else s1.student end  ) student 
+from Seat S1 left outer join Seat S2 on S1.id+1 = S2.id left outer join Seat S3 on S1.id-1 = S3.id
