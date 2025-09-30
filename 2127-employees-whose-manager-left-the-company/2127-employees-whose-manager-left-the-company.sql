dfc61d550942422fc30a48de476bfc5f2da1e05e
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2025-09-30 14:17:37 +0300

    Time: 400 ms (63.73%), Space: 0B (100%) - LeetHub

--- 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -1,6 +1,6 @@
 /* Write your T-SQL query statement below */
-select
-employee_id 
+
+select employee_id 
 from Employees 
-where salary < 30000 and ( manager_id  ) not in (select employee_id  from Employees )
-order by employee_id 
+where salary < 30000 and manager_id not in (select employee_id  from Employees )
+order by employee_id
