1ccec9b2aa412c202ac515adcecef84280914384
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2026-06-26 09:25:58 +0300

    Time: 425 ms (65.96%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,7 @@
-# Write your MySQL query statement below
-select W1.id
-from Weather W1 join Weather W2
-on  datediff(w1.recordDate ,w2.recordDate ) = 1 and  w1.temperature  > w2.temperature 
+/* Write your T-SQL query statement below */
+
+
+select w1.id
+from Weather w1 join Weather w2
+on DATEADD(day, 1,  w2.recordDate)  = w1.recordDate
+where w1.temperature  > w2.temperature 
