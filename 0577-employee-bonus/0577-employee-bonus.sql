7d95cf753447afe8ae14b2bac57e3e458500d310
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2026-06-26 09:54:06 +0300

    Time: 679 ms (42.06%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,4 +1,7 @@
+/* Write your T-SQL query statement below */
+
+
 select name , bonus 
-from Employee E left outer join Bonus B
-on E.empId  = B.empId 
+from Employee E left outer join bonus  B
+on E.empid = B.empid
 where bonus < 1000 or bonus is null
